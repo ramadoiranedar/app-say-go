f6b3d31b31334b03f43340c7c9cf2e46531fc44a
Raden Ario Damar <ramadoiranedar@gmail.com>
2022-08-06 20:40:41 +0700

    Initial Commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/ramadoiranedar/app-say-go
+
+go 1.18
+
+require github.com/ramadoiranedar/go-modules v1.2.0 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	go_modules "github.com/ramadoiranedar/go-modules"
+)
+
+func main() {
+	fmt.Println(go_modules.GoModules())
+}
